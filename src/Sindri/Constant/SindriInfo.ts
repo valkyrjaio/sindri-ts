e5a8bdb593934b9efb92012ee9d4c91fabb865c3
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-21 06:09:18 +0000

    [Release] Update SindriInfo for v26.1.1 release

--- src/Sindri/Constant/SindriInfo.ts
+++ src/Sindri/Constant/SindriInfo.ts
@@ -8,8 +8,8 @@
  */
 
 export class SindriInfo {
-    static readonly VERSION = '26.1.0' as const;
-    static readonly VERSION_BUILD_DATE_TIME = 'June 17 2026 22:16:53 MST' as const;
+    static readonly VERSION = '26.1.1' as const;
+    static readonly VERSION_BUILD_DATE_TIME = 'June 20 2026 23:07:29 MST' as const;
     static readonly ICON = `\
     ██████████
     ████  ████
